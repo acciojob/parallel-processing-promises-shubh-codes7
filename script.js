76cc3100c660a81b367db490b72ff5e2f789287e
shubh-codes7 <shubhamcodes7@gmail.com>
2025-04-02 10:48:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImg(imgArr){
+	imgArr.forEach(img => {
+		output.innerHTML += `<img src=${img} >`
+	})
+}
+
+let promises = images.map(img => {
+	return new Promise(res => res(img.url))
+})
+
+Promise.all(promises).then(downloadImg)
+
+
+
